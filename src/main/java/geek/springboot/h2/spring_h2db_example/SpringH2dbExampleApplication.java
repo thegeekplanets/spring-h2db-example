4d4d8742bcb168655c3fc8ee4bceeb963450bbc2
Hari Kanure <harikanure007@gmail.com>
2025-02-10 11:53:22 +0530

    initial commit

---
+++ src/main/java/geek/springboot/h2/spring_h2db_example/SpringH2dbExampleApplication.java
@@ -0,0 +1,13 @@
+package geek.springboot.h2.spring_h2db_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringH2dbExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringH2dbExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/geek/springboot/h2/spring_h2db_example/SpringH2dbExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package geek.springboot.h2.spring_h2db_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringH2dbExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
